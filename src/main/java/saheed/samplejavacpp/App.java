fe656f5447433c62665aa46c7df04b9ef36bce40
Bolarinwa Saheed Olayemi <refactormyself@users.noreply.github.com>
2019-06-14 08:41:47 +0200

    Math class removed and resource path updated

--- src/main/java/saheed/samplejavacpp/App.java
+++ src/main/java/saheed/samplejavacpp/App.java
@@ -14,5 +14,16 @@ public class App
         System.out.println("subtracting is " + sub);
         System.out.println("multiplying is " + mult);
         
+        /*
+        int add1 = new Math().add(100, 333);
+        int sub1 = new Math().subtract(100, 333);
+        int mult1 = new Math().multiply(100, 333);
+        
+        System.out.println("\nNow using Math.h : \n");
+        System.out.println("adding is " + add1);
+        System.out.println("subtracting is " + sub1);
+        System.out.println("multiplying is " + mult1);
+        */
+        
     }
 }
